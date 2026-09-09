ecda4a6416c562ec10ccfefd31f4407088a881ed
Blake Lemarr <slemarr@fingerlakes.edu>
2026-09-09 01:22:42 -0400

    created the hello class from CSC-190 Lecture

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * @author Blake Lemarr
+ * @version 09.09.26
+ *
+ * Prints "Hello!"
+ * Created to get familiar with IntelliJ IDE
+ *
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
