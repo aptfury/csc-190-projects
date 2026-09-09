42a6044ec7e8acd90416fd8e67255b5b9d9ebf60
Blake Lemarr <slemarr@fingerlakes.edu>
2026-09-09 02:34:12 -0400

    refactored/abstracted the coding in main() to methods for proper code styling

--- src/LetterCount.java
+++ src/LetterCount.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author Blake
  * @version 09.09.26
@@ -5,31 +7,51 @@
  * Counts the number of letters in a given word
  */
 
-// imports
-import java.util.Scanner;
-
-// class
 public class LetterCount {
+    /**
+     * @param args [any] - no arguments needed or used
+     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter a word: ");
-        String word = input.next();
+        String word = requestInput(input, "Enter a Word: ");
+        char ch = requestInput(input, "Enter a letter: ").charAt(0);
 
-        System.out.println("Enter a letter: ");
-        char letter = input.next().charAt(0);
+        int count = letterCounter(word, ch);
 
+        System.out.println("The letter '" + ch + "' appears in the word '" + word + "' " + count + " times.");
+    }
+
+    /**
+     * Requests a string input from the user. Ideally this would be modified to account for no input/empty string
+     * values, but I am not putting that much effort into an ungraded practice assignment.
+     *
+     * @param input [Scanner] - the scanner variable set up in main
+     * @param req [String] - the message for the prompt provided to the user
+     * @return [String] - the response from the user
+     */
+    public static String requestInput(Scanner input, String req) {
+        System.out.println(req);
+
+        return input.next();
+    }
+
+    /**
+     * Counts the number of times that the provided char appears in the provided word
+     *
+     * @param word [String] - the word being searched
+     * @param ch [char] - the character being counted
+     * @return [int] - the number of times the character appears
+     */
+    public static int letterCounter(String word, char ch) {
         int count = 0;
 
         for (int i = 0; i < word.length(); i++) {
-            if (word.toLowerCase().charAt(i) == letter) {
-                count++;
-            }
-            else if (word.toUpperCase().charAt(i) == letter) {
+            if (word.toLowerCase().charAt(i) == ch || word.toUpperCase().charAt(i) == ch) {
                 count++;
             }
         }
 
-        System.out.println("The letter '" + letter + "' appears in the word '" + word + "' " + count + " times.");
+        return count;
     }
 }
